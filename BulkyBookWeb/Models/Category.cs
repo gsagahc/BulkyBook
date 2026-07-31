1bfaa53f2057d0a0c5bea34f0a939908c3887609
gsagahc <gsagahc@hotmail.com>
2026-07-31 13:06:44 -0300

    Incluindo mais validações no método Create.

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -11,7 +11,7 @@ namespace BulkyBookWeb.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; } = string.Empty;
-
+        [Range (0,100, ErrorMessage = "Display order must be between 0 and 100")]
         public int DisplayOrder { get; set; } 
         
         public int OrderID { get; set; }
