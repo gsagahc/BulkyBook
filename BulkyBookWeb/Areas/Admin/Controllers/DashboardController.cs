5aba948b470f676116e1c90c082d5654f1e1f04a
Guilherme Chagas <gsagahc@hotmail.com>
2026-08-21 20:14:29 -0300

    Iniciando implementação de Admin Dashboard e ShoppingCart

---
+++ BulkyBookWeb/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin, Employee")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
