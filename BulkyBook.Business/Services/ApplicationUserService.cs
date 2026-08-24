9bd909e115bbf746f4d0a79d5726a78310520fc2
Guilherme Chagas <gsagahc@hotmail.com>
2026-08-24 17:10:57 -0300

    Corrigindo o caminho do ApplicationUserService

---
+++ BulkyBook.Business/Services/ApplicationUserService.cs
@@ -0,0 +1,69 @@
+using BulkyBook.Business.Services.IServices;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks; // added
+
+namespace BulkyBook.Business.Services
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        private readonly ApplicationDbContext _context;
+     
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ApplicationUser> AddRegister(RegisterVM registerVM)
+        {
+           var user = new ApplicationUser
+           {
+               UserName = registerVM.Email,
+               Email = registerVM.Email,
+               Name = registerVM.Name,
+               StreetAddress = registerVM.StreetAddress,
+               City = registerVM.City,
+               State = registerVM.State,
+               PostalCode = registerVM.PostalCode,
+               Country = registerVM.Country,
+               PhoneNumber = registerVM.PhoneNumber
+           };
+            return Task.FromResult(user);
+        }
+
+        public async Task<ApplicationUser> CreateApplicationUserAsync(ApplicationUser user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    
+
+
+         
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.Where(u => u.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task<ApplicationUser?> GetUserByIdAsync(string userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+       
+    }
+}
---
+++ BulkyBook.Business/Services/IServices/IApplicationUserService.cs
@@ -0,0 +1,17 @@
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public interface IApplicationUserService
+    {
+        Task<ApplicationUser> CreateApplicationUserAsync(ApplicationUser user);
+        Task<ApplicationUser> AddRegister(RegisterVM registerVM);
+        Task<ApplicationUser?> GetUserByIdAsync(string userId);
+        Task<IEnumerable<ApplicationUser>> GetAllUsersAsync();
+        Task<ApplicationUser> GetUserByEmailAsync(string email);
+    }
+}
